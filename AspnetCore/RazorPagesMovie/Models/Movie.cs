83eeed1531232631cafb651a7b5ab5dbe454833e
unknown <yiyanwei@live.com>
2019-11-25 21:24:51 +0800

    '提交model验证特性，包括Range，RegularExpression，DisplayFormat，StringLength等'

--- AspnetCore/RazorPagesMovie/Models/Movie.cs
+++ AspnetCore/RazorPagesMovie/Models/Movie.cs
@@ -8,13 +8,18 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID{get;set;}
+        [StringLength(30,MinimumLength=3,ErrorMessage="标题长度需大于3个字符小于30个字符")]
         public string Title{get;set;}
         [DataType(DataType.Date)]
+        [Range(typeof(DateTime),"1/1/1966","1/1/2020",ErrorMessage="请输入正确的时间范围")]
         [Display(Name="Release Date")]
         public DateTime ReleaseDate{get;set;}
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"),Required,StringLength(30)]
         public string Genre{get;set;}
+        [Range(1,100),DataType(DataType.Currency)]
         [Column(TypeName="decimal(18,2)")]
         public decimal Price{get;set;}
+        [RegularExpression(@"^[A-Z][a-zA-Z0-9""'\s-]*$"),StringLength(5)]
         public string Rating{get;set;}
     }
 }
