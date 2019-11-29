21883a44a3c6aa84a658df9cf42d4f314d88907f
unknown <yiyanwei@live.com>
2019-11-29 21:19:51 +0800

    add models to project

---
+++ AspnetCore/BooksApi/Models/Book.cs
@@ -0,0 +1,19 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BooksApi.Models
+{
+    public class BooksApi{
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id{get;set;}
+        [BsonElement("Name")]
+        public string BookName{get;set;}
+
+        public decimal Price{get;set;}
+
+        public string Category{get;set;}
+
+        public string Author{get;set;}
+    }
+}
