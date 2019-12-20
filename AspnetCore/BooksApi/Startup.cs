cc49de309b355c8e9ec4738208d48280c6151da6
陈舒艺 <chenshuyi@vip56.cn>
2019-12-20 13:14:30 +0800

    config the enviroment for Swagger UI，include what set up to generator the comments of xml

--- AspnetCore/BooksApi/Startup.cs
+++ AspnetCore/BooksApi/Startup.cs
@@ -1,11 +1,10 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +12,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using BooksApi.Models;
 using BooksApi.Services;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace BooksApi
 {
@@ -29,12 +30,28 @@ namespace BooksApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<BookstoreDatabaseSettings>(Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
-            services.AddSingleton<IBookstoreDatabaseSettings>(sp=>sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
+            services.AddSingleton<IBookstoreDatabaseSettings>(sp => sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
             services.AddControllers()
             //使用大驼峰序列化json
-            .AddNewtonsoftJson(options=>options.UseMemberCasing());
+            .AddNewtonsoftJson(options => options.UseMemberCasing());
             //添加BookService到依赖注入，单例模式,因为mongo client的重用原则
             services.AddSingleton<BookService>();
+
+            //add swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v2", new OpenApiInfo { Title = "My API2", Version = "v2", Contact = new OpenApiContact { Name = "yiyanwei", Url = new Uri("http://www.baidu.com"), Email = "yiyanwei@live.com" } });
+                //为 Swagger JSON and UI设置xml文档注释路径
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1.0", new Info { Title = "My Demo API", Version = "1.0" });
+            //     c.IncludeXmlComments(System.IO.Path.Combine(System.AppContext.BaseDirectory, "ZhiKeCore.API.xml"));
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +71,21 @@ namespace BooksApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // app.UseSwagger();
+            // app.UseSwaggerUI(c =>
+            // {
+            //     c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "My Demo API (V 1.0)");
+            // });
+
+            //启用中间件服务生成Swagger作为JSON终结点
+            app.UseSwagger();
+            //启用中间件服务对swagger-ui，指定Swagger JSON终结点
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API V2");
             });
         }
     }
