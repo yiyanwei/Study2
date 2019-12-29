6d1e6d92b9045291ed09844f90325de2e89187a1
yiyanwei <yiyanwei@live.com>
2019-12-29 20:58:58 +0800

    delete the file type of dll,and add weixiapplet project

---
+++ WeixinApplet/Demo_20191229/miniprogram/pages/news/news.js
@@ -0,0 +1,18 @@
+"use strict";
+Page({
+  data: {
+    name: "夷延卫",
+    age: 31,
+    sex: '男',
+    Courses: [{
+        CourseName: "Asp.net Core2.0",
+        CoursePeriod: 60
+      },
+      {
+        CourseName: "TypeScript",
+        CoursePeriod: 30
+      }
+    ]
+  }
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmV3cy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm5ld3MudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLElBQUksQ0FBQztJQUNILElBQUksRUFBRTtRQUNKLElBQUksRUFBRSxLQUFLO1FBQ1gsR0FBRyxFQUFFLEVBQUU7UUFDUCxHQUFHLEVBQUUsR0FBRztLQUNUO0NBQ0YsQ0FBQyxDQUFBIiwic291cmNlc0NvbnRlbnQiOlsiUGFnZSh7XG4gIGRhdGE6IHtcbiAgICBuYW1lOiBcIuWkt+W7tuWNq1wiLFxuICAgIGFnZTogMzEsXG4gICAgc2V4OiAn55S3J1xuICB9XG59KSJdfQ==
---
+++ WeixinApplet/Demo_20191229/miniprogram/pages/news/news.ts
@@ -0,0 +1,17 @@
+Page({
+  data: {
+    name: "夷延卫",
+    age: 31,
+    sex: '男',
+    Courses: [
+      {
+        CourseName: "Asp.net Core2.0",
+        CoursePeriod: 60
+      },
+      {
+        CourseName: "TypeScript",
+        CoursePeriod: 30
+      }
+    ]
+  }
+})
