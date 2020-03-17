432d56d630ef1b1fdbd0043efac3343fd02af6fb
yiyanwei <yiyanwei@live.com>
2020-03-17 21:39:01 +0800

    添加图片

--- Vue/onlineordermanage/src/components/product/AddPro.vue
+++ Vue/onlineordermanage/src/components/product/AddPro.vue
@@ -20,7 +20,31 @@
       <el-input v-model="form.proBaseUnit" style="width:80%;"></el-input>
     </el-form-item>
     <el-form-item label="产品图片">
-      <el-upload v-if="uploadVisible"
+      <el-upload action="#" list-type="picture-card" :auto-upload="false">
+        <i slot="default" class="el-icon-plus"></i>
+        <div slot="file" slot-scope="{file}">
+          <img class="el-upload-list__item-thumbnail" :src="file.url" alt />
+          <span class="el-upload-list__item-actions">
+            <span class="el-upload-list__item-preview" @click="handlePictureCardPreview(file)">
+              <i class="el-icon-zoom-in"></i>
+            </span>
+            <span
+              v-if="!disabled"
+              class="el-upload-list__item-delete"
+              @click="handleDownload(file)"
+            >
+              <i class="el-icon-download"></i>
+            </span>
+            <span v-if="!disabled" class="el-upload-list__item-delete" @click="handleRemove(file)">
+              <i class="el-icon-delete"></i>
+            </span>
+          </span>
+        </div>
+      </el-upload>
+      <el-dialog :visible.sync="dialogVisible">
+        <img width="100%" :src="dialogImageUrl" alt />
+      </el-dialog>
+      <!-- <el-upload v-if="uploadVisible"
         action="https://jsonplaceholder.typicode.com/posts/"
         list-type="picture-card"
         :limit="1"
@@ -32,7 +56,7 @@
       </el-upload>
       <el-dialog :visible.sync="dialogVisible">
         <img width="100%" :src="dialogImageUrl" alt />
-      </el-dialog>
+      </el-dialog>-->
     </el-form-item>
     <el-form-item label="产品描述">
       <el-input type="textarea" v-model="form.proDesc"></el-input>
@@ -48,18 +72,21 @@ import http from "../../common/http/vueresource.js";
 export default {
   data() {
     return {
-      uploadVisible:true,
+      dialogImageUrl: "",
+      dialogVisible: false,
+      disabled: false,
+      uploadVisible: true,
       procateoptions: [],
       form: {
         proName: "",
         proCode: "",
         proBaseUnit: "",
         categoryId: "",
-        proDesc:""
+        proDesc: ""
       }
     };
   },
-  mounted(){
+  mounted() {
     this.getCategoryList();
   },
   methods: {
@@ -73,15 +100,22 @@ export default {
         }
       });
     },
-    uploadSuccess(a){
+    uploadSuccess(a) {
       console.log(a);
     },
     // handlePictureCardPreview(a){
     //   console.log(a);
     //   return a;
     // },
-    handleRemove(a){
-      console.log(a);
+    handleRemove(file) {
+      console.log(file);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
+    handleDownload(file) {
+      console.log(file);
     },
     onSubmit() {
       console.log("submit!");
