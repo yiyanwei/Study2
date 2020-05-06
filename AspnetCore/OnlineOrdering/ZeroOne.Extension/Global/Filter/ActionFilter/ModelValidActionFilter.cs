e5415a94d9b9dccf7670478e781fbb6fb0a289c0
yiyanwei <yiyanwei@live.com>
2020-05-06 10:56:33 +0800

    修改表单验证过滤

--- AspnetCore/OnlineOrdering/ZeroOne.Extension/Global/Filter/ActionFilter/ModelValidActionFilter.cs
+++ AspnetCore/OnlineOrdering/ZeroOne.Extension/Global/Filter/ActionFilter/ModelValidActionFilter.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ZeroOne.Extension
 {
+    /// <summary>
+    /// 表单对象验证过滤器
+    /// </summary>
     public class ModelValidActionFilter : IActionFilter
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
         public void OnActionExecuted(ActionExecutedContext context)
         {
 
@@ -20,13 +28,33 @@ namespace ZeroOne.Extension
         {
             if (!context.ModelState.IsValid)
             {
-                StringBuilder sbError = new StringBuilder();
-                foreach (var item in context.ModelState.Values)
+                List<string> totalErrList = new List<string>();
+                foreach (var key in context.ModelState.Keys)
                 {
-                    foreach (var error in item.Errors)
+                    var item = context.ModelState[key];
+                    if (item.ValidationState == ModelValidationState.Invalid)
                     {
-                        sbError.Append(error.ErrorMessage);
+                        List<string> errMsgList = new List<string>();
+                        foreach (var error in item.Errors)
+                        {
+                            if (string.IsNullOrEmpty(error?.ErrorMessage))
+                            {
+                                if (!string.IsNullOrEmpty(error.Exception?.Message))
+                                {
+                                    errMsgList.Add(error.Exception.Message);
+                                }
+                            }
+                            else
+                            {
+                                errMsgList.Add(error.ErrorMessage);
+                            }
+                        }
+                        totalErrList.Add($"{key}:{string.Join('|', errMsgList)}");
                     }
+                }
+                if (totalErrList.Count > 0)
+                {
+                    throw new Exception(string.Join('\n', totalErrList));
                 }
             }
         }
